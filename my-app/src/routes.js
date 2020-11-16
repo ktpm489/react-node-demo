c535350c5c4cb4b0d36b3ba70539c3136771c5c2
Hoàng Vũ Nguyễn <hoangvuit893@gmail.com>
2020-11-16 23:56:24 +0700

    ok

---
+++ my-app/src/routes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import NotFound from './components/NotFound';
+
+const routes = [
+    {
+        path : '/',
+        exact : true,
+        main : () => <Home />
+    },
+    {
+        path : '/about',
+        exact : false,
+        main : () => <About />
+    },
+    {
+        path : '/notfound',
+        exact : false,
+        main : () => <NotFound />
+    }
+   
+];
+
+export default routes;
